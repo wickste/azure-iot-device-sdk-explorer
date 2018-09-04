8d9ffcb1acf21128f4254a7edf52e0302cd9f9c6
Stefan Wick <swick@microsoft.com>
2018-09-03 21:45:57 -0700

    Initial commit

---
+++ Azure IoT Device SDK Explorer/App.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using Azure_IoT_Device_SDK_Explorer.Services;
+
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+
+namespace Azure_IoT_Device_SDK_Explorer
+{
+    public sealed partial class App : Application
+    {
+        private Lazy<ActivationService> _activationService;
+
+        private ActivationService ActivationService
+        {
+            get { return _activationService.Value; }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+
+            // TODO WTS: Add your app in the app center and set your secret here. More at https://docs.microsoft.com/en-us/appcenter/sdk/getting-started/uwp
+            AppCenter.Start("{Your App Secret}", typeof(Analytics), typeof(Crashes));
+
+            // Deferred execution until used. Check https://msdn.microsoft.com/library/dd642331(v=vs.110).aspx for further info on Lazy<T> class.
+            _activationService = new Lazy<ActivationService>(CreateActivationService);
+        }
+
+        protected override async void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            if (!args.PrelaunchActivated)
+            {
+                await ActivationService.ActivateAsync(args);
+            }
+        }
+
+        protected override async void OnActivated(IActivatedEventArgs args)
+        {
+            await ActivationService.ActivateAsync(args);
+        }
+
+        private ActivationService CreateActivationService()
+        {
+            return new ActivationService(this, typeof(Views.ConnectPage), new Lazy<UIElement>(CreateShell));
+        }
+
+        private UIElement CreateShell()
+        {
+            return new Views.ShellPage();
+        }
+    }
+}
---
+++ Azure IoT Device SDK Explorer/Views/ShellPage.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+using Azure_IoT_Device_SDK_Explorer.Helpers;
+using Azure_IoT_Device_SDK_Explorer.Services;
+
+using Windows.Foundation.Metadata;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Azure_IoT_Device_SDK_Explorer.Views
+{
+    // TODO WTS: Change the icons and titles for all NavigationViewItems in ShellPage.xaml.
+    public sealed partial class ShellPage : Page, INotifyPropertyChanged
+    {
+        private NavigationViewItem _selected;
+
+        public NavigationViewItem Selected
+        {
+            get { return _selected; }
+            set { Set(ref _selected, value); }
+        }
+
+        public ShellPage()
+        {
+            InitializeComponent();
+            HideNavViewBackButton();
+            DataContext = this;
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            NavigationService.Frame = shellFrame;
+            NavigationService.Navigated += Frame_Navigated;
+            KeyboardAccelerators.Add(ActivationService.AltLeftKeyboardAccelerator);
+            KeyboardAccelerators.Add(ActivationService.BackKeyboardAccelerator);
+        }
+
+        private void Frame_Navigated(object sender, NavigationEventArgs e)
+        {
+            if (e.SourcePageType == typeof(SettingsPage))
+            {
+                Selected = navigationView.SettingsItem as NavigationViewItem;
+                return;
+            }
+
+            Selected = navigationView.MenuItems
+                            .OfType<NavigationViewItem>()
+                            .FirstOrDefault(menuItem => IsMenuItemForPageType(menuItem, e.SourcePageType));
+        }
+
+        private bool IsMenuItemForPageType(NavigationViewItem menuItem, Type sourcePageType)
+        {
+            var pageType = menuItem.GetValue(NavHelper.NavigateToProperty) as Type;
+            return pageType == sourcePageType;
+        }
+
+        private void OnItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked)
+            {
+                NavigationService.Navigate(typeof(SettingsPage));
+                return;
+            }
+
+            var item = navigationView.MenuItems
+                            .OfType<NavigationViewItem>()
+                            .First(menuItem => (string)menuItem.Content == (string)args.InvokedItem);
+            var pageType = item.GetValue(NavHelper.NavigateToProperty) as Type;
+            NavigationService.Navigate(pageType);
+        }
+
+        private void HideNavViewBackButton()
+        {
+            if (ApiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract", 6))
+            {
+                navigationView.IsBackButtonVisible = NavigationViewBackButtonVisible.Collapsed;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
